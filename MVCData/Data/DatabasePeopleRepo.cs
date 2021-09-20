9e33032ca20d05c86a88e06902e0bf0be9e9fee9
Marja Rolandsson <marja.rolandsson@gmail.com>
2021-09-20 14:42:14 +0200

    Created DatabasePeopleRepo class

---
+++ MVCData/Data/DatabasePeopleRepo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCData.Models;
+
+namespace MVCData.Data
+{
+    public class DatabasePeopleRepo : IPeopleRepo
+    {
+        public Person Create(string name, string city, int phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
